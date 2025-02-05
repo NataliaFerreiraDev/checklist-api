c9bc0e19f5f8f0a81cf3b377e8412472d60527bc
Natalia Ferreira <nataliaferreira.dev@yahoo.com>
2025-02-05 07:39:01 -0300

    Criacao dos repositorios: CategoryEntity e ChecklistItemEntity.

--- src/main/java/br/com/checklist_api/repository/CategoryRepository.java
+++ src/main/java/br/com/checklist_api/repository/CategoryRepository.java
@@ -2,12 +2,14 @@ package br.com.checklist_api.repository;
 
 import br.com.checklist_api.domain.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Repositório para acesso à entidade Category no banco de dados.
  */
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
     Optional<CategoryEntity> findByGuid(String guid);
--- src/main/java/br/com/checklist_api/repository/ChecklistItemRepository.java
+++ src/main/java/br/com/checklist_api/repository/ChecklistItemRepository.java
@@ -2,12 +2,14 @@ package br.com.checklist_api.repository;
 
 import br.com.checklist_api.domain.ChecklistItemEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Repositório para acesso à entidade Checklist Item no banco de dados.
  */
+@Repository
 public interface ChecklistItemRepository extends JpaRepository<ChecklistItemEntity, Long> {
 
     Optional<ChecklistItemEntity> findByGuid(String guid);
